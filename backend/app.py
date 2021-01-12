b2c4081fea2a653cbe446a02fef069cfecd1dc23
James <james.a.lubin@gmail.com>
2021-01-12 10:18:39 -0500

    Add gitignore to backend directory and fix vcs.xml

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, session, render_template, request, flash, redirect, json, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return "world"
+
+@app.route('/games')
+def games():
+    return jsonify({
+        "Fort Totten 21": "A",
+        "North Hills Run": "Data"
+    })
